fe284d4885860ed74dd6b80fbd5cf5dfde57c3b1
dramane188 <72475690+dramane188@users.noreply.github.com>
2021-03-25 22:13:55 -0400

    CREATE TABLE

---
+++ trees_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE new_york_tree_census_data (
+id Serial PRIMARY KEY,
+tree_id INT,
+stump_diam INT,
+status VARCHAR(255), 
+health VARCHAR(255), 
+spc_latin VARCHAR(255), 
+spc_common VARCHAR(255), 
+address VARCHAR(255), 
+zipcode INT, 
+boroname VARCHAR(255), 
+nta_name VARCHAR(255), 
+latitude FLOAT, 
+longitude FLOAT
+);
+
