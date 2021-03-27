f66eccfe602282e646f387c2b81449ef91d9762f
Shadee Tabasi <shadeetabasi@gmail.com>
2021-03-27 14:08:22 -0400

    leaflet attempts

---
+++ HTML-CSS-v2/assets/js/logic2.js
@@ -0,0 +1,18 @@
+var newYorkCoords = [40.73, -74.0059];
+var mapZoomLevel = 12;
+
+// Create the createMap function
+var myMap = L.map("leafletmap", {
+  center: [40.73, -74.0059],
+  zoom: 12
+});
+
+// // Create the tile layer that will be the background of our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
